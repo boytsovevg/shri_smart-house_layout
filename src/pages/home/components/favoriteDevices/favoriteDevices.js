1affcad473d6561a9d89e186c3689688fa323160
Eugene <evgeniy.boytsov@gmail.com>
2018-08-13 21:18:43 +0300

    Добавил структуру для favorite devices

--- src/pages/home/components/favoriteDevices/favoriteDevices.js
+++ src/pages/home/components/favoriteDevices/favoriteDevices.js
@@ -1,6 +1,7 @@
 import './favoriteDevices.scss';
 
 import { categories } from '../categories/categories';
+import { deviceList } from '../../../../components';
 
 export function favoriteDevices() {
 
@@ -12,17 +13,48 @@ export function favoriteDevices() {
         'Камеры'
     ];
 
-    const favoriteDevices = []
+    const favoriteDevices = [
+        {
+            description: 'Xiaomi Yeelight LED Smart Bulb',
+            status: 'Включено'
+        },
+        {
+            description: 'D-link Omna 180 Cam',
+            status: 'Включится в 17:00'
+        },
+        {
+            description: 'Elgato Eve Degree Connected',
+            status: 'Выключено до 17:00'
+        },
+        {
+            description: 'LIFX Mini Day & Dusk A60 E27',
+            status: 'Включится в 17:00'
+        },
+        {
+            description: 'Xiaomi Mi Air Purifier 2S',
+            status: 'Включено'
+        },
+        {
+            description: 'Philips Zhirui',
+            status: 'Выключено'
+        },
+        {
+            description: 'Xiaomi Yeelight LED Smart Bulb',
+            status: 'Включено'
+        }
+    ]
+
+    
 
     return `
         <div class="favorite">
+            
             <div class="favorite__categories">
                 <h1>Избранные устройства</h1>
                 ${categories(categoriyLabels)}
             </div>
-            <div class="favorite__devices">
 
-            </div>
+            ${deviceList(favoriteDevices, { devices: 'favorite__devices', device: 'favorite__device' })}
         </div>
     `
 }
